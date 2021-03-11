f01b189695dda25f8f0711b412a9d09c1bb9eafa
Sumit Govil <sumit.govil@optus.com.au>
2021-03-12 06:54:04 +1100

    mongodb-connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
+
+const uri = `${process.env.MONGODB_START}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_END}`;
+mongoose.connect(
+  uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Database connected!");
+  }
+);
 
 app.get("/", (req: any, res: any) => {
   res.send("HELLOOO");
