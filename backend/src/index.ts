4385e72410cf175b835335a85ad27b26c3a8fac3
Sumit Govil <sumit.govil@optus.com.au>
2021-03-12 07:04:53 +1100

    backend middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import passport from "passport";
+import session from "express-session";
 
 const app = express();
+
+// .env initialisation
 dotenv.config();
 
+// DB Connection
 const uri = `${process.env.MONGODB_START}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_END}`;
 mongoose.connect(
   uri,
@@ -14,8 +20,26 @@ mongoose.connect(
   }
 );
 
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(
+  session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get("/", (req: any, res: any) => {
-  res.send("HELLOOO");
+  res.send("HELLOOO!!!");
 });
 
 app.listen(4000, () => {
